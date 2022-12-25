f7d5b74779c7065b400036c5fbdf48df727c7409
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2022-12-25 15:14:30 -0800

    restructure filesl

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('its linked')
+let userName = prompt('What is your name');
+console.log(userName)
+let userChoice;
+let computerChoice;
+
+let rockButton = document.getElementById('rockBtn')
+let paperButton = document.getElementById('paperBtn')
+let scissorsButton = document.getElementById('scissorsBtn')
+
+rockButton.addEventListener('click', function (){
+    userChoice = 'rock';
+    console.log(userChoice)
+})
+paperButton.addEventListener('click', function (){
+    userChoice = 'paper';
+    console.log(userChoice)
+})
+scissorsButton.addEventListener('click', function (){
+    userChoice = 'scissors';
+    console.log(userChoice)
+})
